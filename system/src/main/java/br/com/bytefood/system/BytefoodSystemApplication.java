4f05176dc389460bfb54b581af35459842f49d3b
aljsjunca <aljs.junca@gmail.com>
2022-06-19 12:52:11 -0300

    criação do projeto java

---
+++ system/src/main/java/br/com/bytefood/system/BytefoodSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.bytefood.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytefoodSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytefoodSystemApplication.class, args);
+	}
+
+}
